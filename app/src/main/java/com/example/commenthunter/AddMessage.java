7bad46a944a92a8092f44f200e5c43992316c252
FMFluke <lutheus_phidelra@hotmail.com>
2019-03-04 19:37:02 +0200

    Add AddMessage activity

---
+++ app/src/main/java/com/example/commenthunter/AddMessage.java
@@ -0,0 +1,13 @@
+package com.example.commenthunter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_message);
+    }
+}
