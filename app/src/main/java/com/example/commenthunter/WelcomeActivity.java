a8d0086d76dbd103a1858cc04c82c6615bc9786c
Ciprian <>
2019-04-29 15:18:33 +0300

    Status and GMap activities linked to buttons

--- app/src/main/java/com/example/commenthunter/WelcomeActivity.java
+++ app/src/main/java/com/example/commenthunter/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.commenthunter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+//        -> Status Activity
+        Button btn = findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(WelcomeActivity.this, StatusActivity.class);
+                WelcomeActivity.this.startActivity(myIntent);
+            }
+        });
+
+//        -> GMap Activity
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(WelcomeActivity.this, GMapActivity.class);
+                WelcomeActivity.this.startActivity(myIntent);
+            }
+        });
     }
+
 }
